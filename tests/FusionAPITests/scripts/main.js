1e33456eaf4b53a678b77f5fca6e2d61a3a1ed36
Scott Halstvedt <shalstvedt@gmail.com>
2012-07-08 19:24:55 -0600

    added chrome fusion api test

---
+++ tests/FusionAPITests/scripts/main.js
@@ -0,0 +1,57 @@
+function connect(host, port) {
+	tcpClient = new TcpClient(host, port);
+	tcpClient.connect(function() {
+	  tcpClient.addResponseListener(function(data) {
+		$('#divDataRaw').empty();
+		$('#divDataRaw').append(data);
+		if($('#divDataRaw').val() == "") { $('#status').attr("src", "red.png");}
+		else { $('#status').attr("src", "green.png");}
+		});
+	});
+}
+
+$(document).ready(function () {
+	var host = '127.0.0.1';
+	var port = 7500;
+	connect(host, port);
+
+	$('#requests').change(function() {
+		if(this.value == "null")
+		{
+			$('#divDataRaw').empty(); 
+			return;
+		}
+		if(this.value == "/workflow/quit"){
+			if(!confirm('Are you sure you want to quit?')) {return;}
+		}
+		var jsonRequest = {"jsonrpc": "2.0", 
+			"method": this.value,
+			"id" : 123
+		};
+		var stringreq = JSON.stringify(jsonRequest);
+		tcpClient.sendMessage(stringreq, function() {$('#status').attr("src", "red.png");});
+	});
+});
+
+/*
+
+var tcp2 = new TcpClient('127.0.0.1', 7500);
+
+tcp2.connect(function() {
+      tcp2.addResponseListener(function(data) {
+        $('#divDataRaw').empty();
+		$('#divDataRaw').append(data);
+		if($('#divDataRaw').val() == "") { $('#status').attr("src", "red.png");}
+		else { $('#status').attr("src", "green.png");}
+     });
+});
+	
+tcp2.sendMessage(JSON.stringify({"jsonrpc": "2.0", 
+	"method": "nui_list_dynamic",
+	"params": {
+		"hostername" : "blah"
+	},
+	"id" : 123
+}), function() {$('#status').attr("src", "red.png");});
+
+*/
